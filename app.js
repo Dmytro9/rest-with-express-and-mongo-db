e8cfe73ac60f78da49c3e15c0666662712d3b47c
Dmytro9 <dpoliuliuk@gmail.com>
2019-06-17 23:07:19 +0300

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+// Globals
+
+console.log();
+setTimeout();
+clearTimeout();
+setInterval();
+clearInterval(); // ...
+
+// in client all global vars or methods can run as window.setTimeout()
+
+// in node not added automatically => to add vars or methods to globals - global.message = 'some message';
