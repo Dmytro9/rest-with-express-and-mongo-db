457d3b4fc6c578edf5332c8d3afd1bd202195d26
Dmytro9 <dpoliuliuk@gmail.com>
2019-06-23 19:25:11 +0300

    node modules

--- app.js
+++ app.js
@@ -1,11 +1,83 @@
-// Globals
-
-console.log();
-setTimeout();
-clearTimeout();
-setInterval();
-clearInterval(); // ...
+/*
+ * Globals fns:
+ * console.log();
+ * setTimeout();
+ * clearTimeout();
+ * setInterval();
+ * clearInterval(); 
+ **/
 
 // in client all global vars or methods can run as window.setTimeout()
 
 // in node not added automatically => to add vars or methods to globals - global.message = 'some message';
+
+
+/*
+ * Modules (every file in node app)
+ * every var and fn in node app incapsulated in theihr file, where they are located 
+ **/
+
+//console.log(module)  
+
+
+/*
+ * Create Module
+ *  
+**/
+var url = 'http://mylogger.io/log';
+
+function log(message) {
+  console.log(message);
+}
+
+module.exports.log = log;
+module.exports.url = url;
+module.exports.endPoint = url;
+
+// or exporting only one var or fn => 
+module.exports = log;
+
+
+
+/*
+ * Loading the Module
+ * return an object of exported prop (fns and vars) or if exported one prop => then just var or fn
+**/
+
+const logger = require('./2');
+
+
+
+/*
+ * Path Module (build in module (require('path'!!)) )
+ * 
+**/
+const path = require('path');
+
+const pathObject = path.parse(__filename);
+console.log(pathObject);
+
+
+/*
+ * OS Module (build in module)
+ * 
+**/
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+console.log(freeMemory);
+
+
+
+/*
+ * File System Module (build in module)
+ * always use all async in real apps
+**/
+const fs = require('fs');
+
+fs.readdir('./', (err, res) => console.log(res.length));
+
+
+
+
