059f8e513efa2b306c1ec37ea68ee644a9408ca4
Dmytro9 <dpoliuliuk@gmail.com>
2019-06-28 17:57:33 +0300

    init rest service

---
+++ 2.js
@@ -0,0 +1,11 @@
+// REST
+
+// CRUD (http://domain/api/customers)
+
+// GET api/customers / api/customers/1
+
+// PUT api/customers/1 and body:  {name: '', ...}
+
+// DELETE api/customers/1
+
+// POST api/customers body: {name: '', ...}
--- npm.js
+++ npm.js
@@ -1,1 +1,29 @@
 // sudo npm i -g npm@5.5.1
+
+// 4.13.6  Major.Minor.Patch (CurruptAPI(BigChange).NewFeature.BugFix) BugFix - 0 (unstable - without bugfix)
+
+// mongoose: "^4.13.6" // 4.x
+// mongoose: "~4.13.6" // 4.13.x
+// mongoose: "4.13.6" // exact version
+
+
+// to see versions:  npm list --depth=0
+// to see  dependencies of installed package:  npm view mongoose dependencies
+// npm view mongoose versions
+
+// Install a specific version:    npm i mongoose@2.4.2
+
+// npm i -g npm-check-updates
+// ncu (ncu -u) - to update (and npm i)
+// npm outdated (check outdated packages)
+
+// Uninstall
+// npm un mongoose
+
+
+// Publish own package
+// npm adduser or npm login
+// npm publish (use unique name for package (ex. random number))
+
+// Update own package (publish new version)
+// neew change version number (npm version major/minor/patch) and npm publish
