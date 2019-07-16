59089dd7df6fa71b29f3187a8a3b3ee4ea96b34b
Dmytro9 <dpoliuliuk@gmail.com>
2019-07-16 23:31:10 +0300

    Auth users

--- express-demo/index.js
+++ express-demo/index.js
@@ -13,6 +13,7 @@ const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const home = require('./routes/home');
 const mongoose = require('mongoose');
 
@@ -49,6 +50,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 // console.log('Application Name: ' + config.get('name'));
